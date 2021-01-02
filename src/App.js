a8ad7a29e24b695a14ce5dbf5b7afa33e984245e
William Oliveira <1704639@aluno.univesp.br>
2021-01-02 13:59:18 -0300

    set the buttons and it's layout

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native'
+
+import Button from './components/Button'
+
+export default () => {
+ 
+    return  (
+    <SafeAreaView style={styles.container}>
+        <View style={styles.buttons}>
+            <Button label='AC'/>
+            <Button label='/'/>
+            <Button label='7'/>
+            <Button label='8'/>
+            <Button label='9'/>
+            <Button label='*'/>
+            <Button label='4'/>
+            <Button label='5'/>
+            <Button label='6'/>
+            <Button label='-'/>
+            <Button label='1'/>
+            <Button label='2'/>
+            <Button label='3'/>
+            <Button label='+'/>
+            <Button label='0'/>
+            <Button label='.'/>
+            <Button label='='/>
+        </View>        
+    </SafeAreaView>
+)}
+
+const styles = StyleSheet.create ({
+    container: {
+        flex: 1,
+    },
+    buttons: {
+        flexDirection:'row',
+        flexWrap: 'wrap'
+    }
+})
+
+
+/*
+*Comando CMD para rodar o emulador android: 
+npx react-native start
+npx react-native run-android
+
+?Se não rodar deletar arquivo json que fica na pasta: \android\app\build\intermediates\signing_config\debug\out
+?E rodar os comandos novamente
+*/
